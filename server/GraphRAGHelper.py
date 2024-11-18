033f0a62b3915b1b51ff0ea36d4a3024a9eeca92
QuintineSol <sol.quintine@gmail.com>
2024-11-18 10:23:20 +0100

    implemented graphRAG functions

--- server/GraphRAGHelper.py
+++ server/GraphRAGHelper.py
@@ -1,15 +1,30 @@
 from RAGHelper import RAGHelper
+from relik import Relik
+from relik.inference.data.objects import RelikOutput
 
 class GraphRAGHelper(RAGHelper):
     def __init__(self, logger):
         super().__init__(logger)
         self.logger = logger
-        # Initialize graph-specific components here
+        self.relation_extractor = Relik.from_pretrained("sapienzanlp/relik-relation-extraction-nyt-large")
 
     def construct_graph(self, documents):
-        # TODO: Implement knowledge graph construction logic here
-        pass
+        graph = {}
+        for doc in documents:
+            relik_out: RelikOutput = self.relation_extractor(doc.page_content)
+            for relation in relik_out.relations:
+                subject = relation.subject
+                object = relation.object
+                predicate = relation.predicate
+                if subject not in graph:
+                    graph[subject] = []
+                graph[subject].append((predicate, object))
+        self.graph = graph
 
     def retrieve_documents(self, query):
-        # TODO: Implement graph-based retrieval logic here
-        pass
+        related_docs = []
+        for subject, relations in self.graph.items():
+            if query in subject:
+                for predicate, object in relations:
+                    related_docs.append((subject, predicate, object))
+        return related_docs
