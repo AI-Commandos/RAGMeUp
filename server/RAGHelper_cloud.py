e591511114008777a27b8e2c176f4a0e984cef3b
bkapteijns <bramkapteijns03@gmail.com>
2024-11-26 14:00:06 +0100

    Fixed bug

--- server/RAGHelper_cloud.py
+++ server/RAGHelper_cloud.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+import torch
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
@@ -73,7 +74,10 @@ class RAGHelperCloud(RAGHelper):
     def initialize_embeddings(self):
         """Initialize the embeddings based on the CPU/GPU configuration."""
         embedding_model = os.getenv('embedding_model')
-        model_kwargs = {'device': 'cpu'} if os.getenv('force_cpu') == "True" else {'device': 'cuda'}
+        model_kwargs = {
+            'device': 'mps' if torch.backends.mps.is_available() else 'cuda' if os.getenv(
+                'force_cpu') != "True" else 'cpu'
+        }
         self.logger.info(f"Initializing embedding model {embedding_model} with params {model_kwargs}.")
         return HuggingFaceEmbeddings(model_name=embedding_model, model_kwargs=model_kwargs)
 
