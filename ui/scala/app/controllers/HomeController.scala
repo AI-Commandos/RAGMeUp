f20e4a01587fb9c0a2cf7e80aaa8104b0f1aaf4d
Thomas-Boot <thomasboot02@gmail.com>
2024-12-03 15:23:09 +0100

    poging 100

--- ui/scala/app/controllers/HomeController.scala
+++ ui/scala/app/controllers/HomeController.scala
@@ -2,37 +2,26 @@ package controllers
 
 import javax.inject._
 import play.api._
-import play.api.http.HttpEntity
-import java.nio.file.Paths
 import play.api.libs.json._
 import play.api.mvc._
 import play.api.libs.ws._
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.ExecutionContext
-import scala.language.postfixOps
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 @Singleton
 class HomeController @Inject()(
-    cc: ControllerComponents,
-    config: Configuration,
-    ws: WSClient
-) (implicit ec: ExecutionContext) extends AbstractController(cc) {
+  cc: ControllerComponents,
+  config: Configuration,
+  ws: WSClient
+)(implicit ec: ExecutionContext) // Use the injected ExecutionContext
+  extends AbstractController(cc) {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index(config))
-  }
-
-  def add() = Action.async { implicit request: Request[AnyContent] =>
-    ws
-      .url(s"${config.get[String]("server_url")}/get_documents")
-      .withRequestTimeout(5 minutes)
-      .get()
-      .map(files => {
-        Ok(views.html.add(files.json.as[Seq[String]]))
-      })
+  def searchGraph(query: String, docs: Seq[JsObject]): Future[String] = {
+    Future {
+      "response from graph" // Replace with your logic
+    }
   }
 
   def search() = Action.async { implicit request: Request[AnyContent] =>
@@ -41,84 +30,15 @@ class HomeController @Inject()(
     val history = (json \ "history").as[Seq[JsObject]]
     val docs = (json \ "docs").as[Seq[JsObject]]
 
-    // Call the helper method to search the graph or document database
-    searchGraph(query, docs).map { answer =>
-      Ok(Json.obj("response" -> answer))
-    }.recover {
-      case e: Exception =>
-        Ok(Json.obj("response" -> "Something went wrong, please try again..."))
-    }
-  }
-
-  // Helper method to simulate graph/document search
-  def searchGraph(query: String, docs: Seq[JsObject]): Future[String] = {
-    // Example: Query search logic (replace this with actual search logic in your graph or DB)
-    Future {
-      if (query.toLowerCase.contains("rag")) {
-        "A rag is a piece of cloth, usually old, used for cleaning or wiping."
-      } else {
-        "Sorry, no relevant information found."
-      }
-    }
-  }
-
-  def download(file: String) = Action.async { implicit request: Request[AnyContent] =>
-    ws.url(s"${config.get[String]("server_url")}/get_document")
+    ws.url(s"${config.get[String]("server_url")}/chat")
       .withRequestTimeout(5.minutes)
-      .post(Json.obj("filename" -> file))
-      .map { response =>
-        if (response.status == 200) {
-          // Get the content type and filename from headers
-          val contentType = response.header("Content-Type").getOrElse("application/octet-stream")
-          val disposition = response.header("Content-Disposition").getOrElse("")
-          val filenameRegex = """filename="?(.+)"?""".r
-          val downloadFilename = filenameRegex.findFirstMatchIn(disposition).map(_.group(1)).getOrElse(file)
-
-          // Stream the response body to the user
-          Result(
-            header = ResponseHeader(200, Map(
-              "Content-Disposition" -> s"""attachment; filename="$downloadFilename"""",
-              "Content-Type" -> contentType
-            )),
-            body = HttpEntity.Streamed(
-              response.bodyAsSource,
-              response.header("Content-Length").map(_.toLong),
-              Some(contentType)
-            )
-          )
-        } else {
-          // Handle error cases
-          Status(response.status)(s"Error: ${response.statusText}")
-        }
-      }
-  }
-
-  def upload = Action(parse.multipartFormData) { implicit request =>
-    request.body.file("file").map { file =>
-      val filename = Paths.get(file.filename).getFileName
-      val dataFolder = config.get[String]("data_folder")
-      val filePath = new java.io.File(s"$dataFolder/$filename")
-
-      file.ref.copyTo(filePath)
-
-      Redirect(routes.HomeController.add()).flashing("success" -> "Added CV to the database.")
-    }.getOrElse {
-      Redirect(routes.HomeController.add()).flashing("error" -> "Adding CV to database failed.")
-    }
-  }
-
-  def delete(file: String) = Action.async { implicit request =>
-    ws.url(s"${config.get[String]("server_url")}/delete")
-      .withRequestTimeout(5.minutes)
-      .post(Json.obj("filename" -> file))
-      .map { response =>
-        val deleteCount = (response.json.as[JsObject] \ "count").as[Int]
-        Redirect(routes.HomeController.add())
-          .flashing("success" -> s"File ${file} has been deleted (${deleteCount} chunks in total).")
-      }
-  }
-
-  def feedback() = Action { implicit request: Request[AnyContent] =>
-    Ok(Json.obj())
+      .post(Json.obj(
+        "prompt" -> query,
+        "history" -> history,
+        "docs" -> docs
+      ))
+      .map(response =>
+        Ok(response.json)
+      )
   }
 }
