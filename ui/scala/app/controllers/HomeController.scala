ae3ea590eb77b47e8a055ccc845ca4b214c45d4e
Thomas-Boot <thomasboot02@gmail.com>
2024-12-03 17:29:24 +0100

    122321321

--- ui/scala/app/controllers/HomeController.scala
+++ ui/scala/app/controllers/HomeController.scala
@@ -2,55 +2,95 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.http.HttpEntity
 import play.api.libs.json._
-import play.api.mvc._
 import play.api.libs.ws._
+import play.api.mvc._
 
-import scala.concurrent.{ExecutionContext, Future}
+import java.nio.file.Paths
 import scala.concurrent.duration._
-import scala.language.postfixOps
 import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
 
 @Singleton
 class HomeController @Inject()(
-  cc: ControllerComponents,
-  config: Configuration,
-  ws: WSClient
-)(implicit ec: ExecutionContext) // Use the injected ExecutionContext
-  extends AbstractController(cc) {
+    cc: ControllerComponents,
+    config: Configuration,
+    ws: WSClient
+)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("Index page")
+    Ok(views.html.index(config))
   }
 
   def asyncExample() = Action.async { implicit request: Request[AnyContent] =>
     Future.successful(Ok("Async example"))
   }
 
-  def add() = Action { implicit request: Request[AnyContent] =>
-    Ok("Add page")
+  def add() = Action.async { implicit request: Request[AnyContent] =>
+    ws.url(s"${config.get[String]("server_url")}/get_documents")
+      .withRequestTimeout(5.minutes)
+      .get()
+      .map { response =>
+        Ok(views.html.add(response.json.as[Seq[String]]))
+      }
   }
 
-  def download(file: String) = Action { implicit request: Request[AnyContent] =>
-    Ok(s"Download page for file: $file")
+  def download(file: String) = Action.async { implicit request: Request[AnyContent] =>
+    ws.url(s"${config.get[String]("server_url")}/get_document")
+      .withRequestTimeout(5.minutes)
+      .post(Json.obj("filename" -> file))
+      .map { response =>
+        if (response.status == 200) {
+          val contentType = response.header("Content-Type").getOrElse("application/octet-stream")
+          val disposition = response.header("Content-Disposition").getOrElse("")
+          val filenameRegex = """filename="?(.+)"?""".r
+          val downloadFilename = filenameRegex.findFirstMatchIn(disposition).map(_.group(1)).getOrElse(file)
+
+          Result(
+            header = ResponseHeader(200, Map(
+              "Content-Disposition" -> s"""attachment; filename="$downloadFilename"""",
+              "Content-Type" -> contentType
+            )),
+            body = HttpEntity.Streamed(
+              response.bodyAsSource,
+              response.header("Content-Length").map(_.toLong),
+              Some(contentType)
+            )
+          )
+        } else {
+          Status(response.status)(s"Error: ${response.statusText}")
+        }
+      }
   }
 
-  def delete(file: String) = Action { implicit request: Request[AnyContent] =>
-    Ok(s"Delete page for file: $file")
+  def delete(file: String) = Action.async { implicit request: Request[AnyContent] =>
+    ws.url(s"${config.get[String]("server_url")}/delete")
+      .withRequestTimeout(5.minutes)
+      .post(Json.obj("filename" -> file))
+      .map { response =>
+        val deleteCount = (response.json.as[JsObject] \ "count").as[Int]
+        Redirect(routes.HomeController.add())
+          .flashing("success" -> s"File $file has been deleted ($deleteCount chunks in total).")
+      }
   }
 
-  def upload() = Action { implicit request: Request[AnyContent] =>
-    Ok("Upload page")
+  def upload = Action(parse.multipartFormData) { implicit request =>
+    request.body.file("file").map { file =>
+      val filename = Paths.get(file.filename).getFileName
+      val dataFolder = config.get[String]("data_folder")
+      val filePath = new java.io.File(s"$dataFolder/$filename")
+
+      file.ref.copyTo(filePath)
+
+      Redirect(routes.HomeController.add()).flashing("success" -> "Added CV to the database.")
+    }.getOrElse {
+      Redirect(routes.HomeController.add()).flashing("error" -> "Adding CV to database failed.")
+    }
   }
 
   def feedback() = Action { implicit request: Request[AnyContent] =>
-    Ok("Feedback page")
-  }
-
-  def searchGraph(query: String, docs: Seq[JsObject])(implicit ec: ExecutionContext): Future[String] = {
-    Future {
-      "response from graph" // Replace with your logic
-    }
+    Ok(Json.obj())
   }
 
   def search() = Action.async { implicit request: Request[AnyContent] =>
@@ -66,8 +106,8 @@ class HomeController @Inject()(
         "history" -> history,
         "docs" -> docs
       ))
-      .map(response =>
+      .map { response =>
         Ok(response.json)
-      )
+      }
   }
 }
