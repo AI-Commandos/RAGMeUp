061faa4ff6c825e58dda8f7cfc655e2083bd8106
bkapteijns <bramkapteijns03@gmail.com>
2024-12-06 12:13:36 +0100

    Don't run in debug mode because it gives problems

--- server/server.py
+++ server/server.py
@@ -245,7 +245,7 @@ def delete_document():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0")
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0")
