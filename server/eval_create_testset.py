0e250ebb309532eedb74e6cffc3992913d979e9e
sjoerdoffringa <sjoerd.offringa@gmail.com>
2024-12-06 09:56:49 +0100

    updated testset script

--- server/eval_create_testset.py
+++ server/eval_create_testset.py
@@ -19,7 +19,6 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 
 
-
 # Load RAG
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -31,32 +30,96 @@ if os.getenv("use_openai") == "True" or os.getenv("use_gemini") == "True" or os.
 else:
     raghelper = RAGHelperLocal(logger)
 
+
 # Set variables from environment
-ragas_use_n_documents = int(os.getenv("vector_store_k"))
+use_n_documents = int(os.getenv("vector_store_k"))
 if os.getenv("rerank") == "True":
-    ragas_use_n_documents = int(os.getenv("rerank_k"))
+    use_n_documents = int(os.getenv("rerank_k"))
 end_string = os.getenv("llm_assistant_token")
-ragas_sample_size = int(os.getenv("ragas_sample_size"))
-ragas_qa_pairs = int(os.getenv("ragas_qa_pairs"))
+sample_size = int(os.getenv("eval_sample_size"))
+n_qa_pairs = int(os.getenv("eval_qa_pairs"))
+
+use_example_questions = os.getenv("eval_use_example_questions").lower() == "true"
+catch_irrelevant_chunks = os.getenv("eval_catch_irrelevant_chunks").lower() == "true"
+check_sample_relevance = os.getenv("eval_check_sample_relevance").lower() == "true"
+retrieve_samples = os.getenv("eval_retrieve_samples").lower() == "true"
+
+
+# configure example questions
+if use_example_questions:
+  example_questions = ast.literal_eval(os.getenv("eval_example_questions"))
+  current_q_query = os.getenv('eval_question_query')
+  prompt_text = os.getenv('eval_example_questions_prompt')
+  for i, question in enumerate(example_questions):
+      prompt_text += f'{i+1}. {question}\n'
+  new_q_query = current_q_query + prompt_text
+  os.environ['eval_question_query'] = new_q_query
+
+# configure catching irrelevant chunks (denying question generation)
+if catch_irrelevant_chunks:
+  current_q_query = os.getenv('eval_question_query')
+  prompt_text = os.getenv('eval_catch_irrelevant_chunks_prompt')
+  new_q_query = f'{current_q_query} {prompt_text}'
+  os.environ['eval_question_query'] = new_q_query
+
+# configure using the same samples as defined testset
+if retrieve_samples:
+  testset_path = f'./testsets/{os.getenv("eval_retrieve_samples_folder")}/'
+
+  # load data
+  testset = pd.read_csv(testset_path + 'testset.csv')
+  testset['true_doc_ids'] = testset['true_doc_ids'].apply(ast.literal_eval)
+  n_qa_pairs = len(testset)
+
+  # Path to chunks
+  testset_chunks_path = testset_path + 'rag_chunks.pickle'
+
+  # Load chunks
+  with open(testset_chunks_path, "rb") as file:
+      rag_chunks = pickle.load(file)
+
+  sampled_docs = testset['true_doc_ids'].apply(
+    lambda ids: [doc for doc in rag_chunks if doc.metadata['id'] in ids]
+    ).to_list()
+
+
 
 # Set up the documents and get a sample
 documents = raghelper.chunked_documents
+document_sample = sample(documents, sample_size)
 
-print(f'Number of documents: {len(documents)}')
-print(f'Number of documents to sample: {ragas_sample_size}')
-document_sample = sample(documents, ragas_sample_size)
+# Prepare template for checking sample relevance
+if check_sample_relevance:
+    if use_cloud:
+        thread = [
+            ('system', os.getenv('eval_sample_relevance_instruction')),
+            ('human', os.getenv('eval_sample_relevance_query'))
+        ]
+        prompt = ChatPromptTemplate.from_messages(thread)
+    else:
+        thread = [
+            {'role': 'system', 'content': os.getenv("eval_sample_relevance_instruction")},
+            {'role': 'user', 'content': os.getenv("eval_sample_relevance_query")}
+        ]
+        prompt_template = raghelper.tokenizer.apply_chat_template(thread, tokenize=False)
+        prompt = PromptTemplate(
+            input_variables=["context"],
+            template=prompt_template,
+        )
+
+    rag_sample_relevance = prompt | raghelper.llm
 
 # Prepare template for generating questions
 if use_cloud:
     thread = [
-        ('system', os.getenv('ragas_question_instruction')),
-        ('human', os.getenv('ragas_question_query'))
+        ('system', os.getenv('eval_question_instruction')),
+        ('human', os.getenv('eval_question_query'))
     ]
     prompt = ChatPromptTemplate.from_messages(thread)
 else:
     thread = [
-        {'role': 'system', 'content': os.getenv("ragas_question_instruction")},
-        {'role': 'user', 'content': os.getenv("ragas_question_query")}
+        {'role': 'system', 'content': os.getenv("eval_question_instruction")},
+        {'role': 'user', 'content': os.getenv("eval_question_query")}
     ]
     prompt_template = raghelper.tokenizer.apply_chat_template(thread, tokenize=False)
     prompt = PromptTemplate(
@@ -69,14 +132,14 @@ rag_question = prompt | raghelper.llm
 # Prepare template for generating answers with our questions
 if use_cloud:
     thread = [
-        ('system', os.getenv('ragas_answer_instruction')),
-        ('human', os.getenv('ragas_answer_query'))
+        ('system', os.getenv('eval_answer_instruction')),
+        ('human', os.getenv('eval_answer_query'))
     ]
     prompt = ChatPromptTemplate.from_messages(thread)
 else:
     thread = [
-        {'role': 'system', 'content': os.getenv("ragas_answer_instruction")},
-        {'role': 'user', 'content': os.getenv("ragas_answer_query")}
+        {'role': 'system', 'content': os.getenv("eval_answer_instruction")},
+        {'role': 'user', 'content': os.getenv("eval_answer_query")}
     ]
     prompt_template = raghelper.tokenizer.apply_chat_template(thread, tokenize=False)
     prompt = PromptTemplate(
@@ -88,9 +151,32 @@ rag_answer = prompt | raghelper.llm
 
 # Create test set
 qa_pairs = []
-for i in range(0, ragas_qa_pairs):
-    selected_docs = random.sample(document_sample, min(ragas_use_n_documents, len(document_sample)))
+qa_generated = 0
+rejected_samples = []
+
+while qa_generated < n_qa_pairs:
+    if retrieve_samples:
+        selected_docs = sampled_docs[qa_generated]
+    else:
+        selected_docs = random.sample(document_sample, min(use_n_documents, len(document_sample)))
     formatted_docs = RAGHelper.format_documents(selected_docs)
+
+    if check_sample_relevance:
+        sample_relevance_chain = ({"context": RunnablePassthrough()} | rag_sample_relevance)
+        response = sample_relevance_chain.invoke(formatted_docs)
+        if use_cloud:
+            if hasattr(response, 'content'):
+                sample_relevance = response.content
+            elif hasattr(response, 'answer'):
+                sample_relevance = response.answer
+            elif 'answer' in response:
+                sample_relevance = response["answer"]
+        else:
+            sample_relevance = response[response.rindex(end_string)+len(end_string):]
+
+        if 'false' in sample_relevance.lower():
+            rejected_samples.append(selected_docs)
+            continue
 
     question_chain = ({"context": RunnablePassthrough()} | rag_question)
     response = question_chain.invoke(formatted_docs)
@@ -120,11 +206,12 @@ for i in range(0, ragas_qa_pairs):
 
     qa_pairs.append({"question": question, "ground_truth": answer, "true_doc_ids": id_set})
 
+    qa_generated += 1
 
 # convert type dataset to pandas dataframe
 df_testset = pd.DataFrame(qa_pairs)
 
-# Variables
+# Save testset in folder
 rag_chunks_path = "rag_chunks.pickle"
 testsets_folder = "testsets"
 
@@ -150,4 +237,4 @@ df_testset.to_csv(testset_csv_path, index=False)
 rag_chunks_copy_path = os.path.join(target_folder, "rag_chunks.pickle")
 shutil.copy(rag_chunks_path, rag_chunks_copy_path)
 
-print(f"Data saved in folder: {target_folder}")
+print(f"Testset saved in folder: {target_folder}")
